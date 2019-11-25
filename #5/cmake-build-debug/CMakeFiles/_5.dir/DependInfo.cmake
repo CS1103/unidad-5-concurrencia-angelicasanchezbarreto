07d65cd54e34e6ddce369ef3fb1ae93bcebbec1e
angelicasanchezbarreto <angelica.sanchez@utec.edu.pe>
2019-11-24 23:44:44 -0500

    7 ejercicios

--- #5/cmake-build-debug/CMakeFiles/_5.dir/DependInfo.cmake
+++ #5/cmake-build-debug/CMakeFiles/_5.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/lica-pc/CLionProjects/POO 2/11.Concurrencia/EJERCICIOS/#5/main.cpp" "/home/lica-pc/CLionProjects/POO 2/11.Concurrencia/EJERCICIOS/#5/cmake-build-debug/CMakeFiles/_5.dir/main.cpp.o"
+  "/home/lica-pc/CLionProjects/git_concurrencia/unidad-5-concurrencia-angelicasanchezbarreto/#5/main.cpp" "/home/lica-pc/CLionProjects/git_concurrencia/unidad-5-concurrencia-angelicasanchezbarreto/#5/cmake-build-debug/CMakeFiles/_5.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- #5/main.cpp
+++ #5/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <vector>
+#include <thread>
+
+using namespace std;
+
+template <typename Container>
+void delete_duplicated(Container &cnt, int num){
+    for(auto it = cnt.begin(); it != cnt.end(); it++){
+            if(*it==num){
+                cnt.erase(it);
+                it--;
+            }
+    }
+
+    for(auto iter = cnt.begin(); iter != cnt.end(); iter++){
+        cout<<*iter<<" ";
+    }
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<int> v1 = {1, 3, 4, 1, 3, 2, 3, 4, 6, 5};
+    thread t1([&v1]{delete_duplicated(v1,3);});
+    t1.join();
+
+    cout<<endl;
+
+    vector<int> v3 = {1, 1, 1, 1, 3, 2, 2, 2, 2, 5};
+    thread t2([&v3]{delete_duplicated(v3,2);});
+    t2.join();
+
     return 0;
 }
