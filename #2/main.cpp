b1c41582924e028533d76391b014a3846208e1d9
angelicasanchezbarreto <angelica.sanchez@utec.edu.pe>
2019-11-24 22:19:36 -0500

    3 ejercicios

---
+++ #2/main.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+void imprimir(vector<int> vec)
+{
+	for(auto i : vec)
+	{
+		cout << i << " "; 
+	}
+}
+
+void incremento(vector<int> &vec, int n)
+{
+	for(auto& i : vec)
+	{
+		i += n;
+	}
+
+	imprimir(vec);
+}
+
+int main(){
+	vector<int> vec = {1, 5, 7, 10, 12, 14, 15, 18, 20, 22, 25, 27, 30, 64, 110, 220};
+	cin >> n;
+	thread t1(incremento, ref(v1), n);
+	t1.join();
+}
